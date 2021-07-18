d08944a2c8a68d48f8290286fbb9f139dab2d5e4
Shir Bar Lev <shirblc@icloud.com>
2021-07-18 20:21:07 +0100

    Added StudentInformation struct.

---
+++ onthemap/Models/StudentInformation.swift
@@ -0,0 +1,22 @@
+//
+//  StudentInformation.swift
+//  onthemap
+//
+//  Created by Shir Bar Lev on 18/07/2021.
+//
+
+import Foundation
+
+struct StudentInformation: Codable {
+    let objectId: String
+    let uniqueKey: String
+    let firstName: String
+    let lastName: String
+    let mapString: String
+    let mediaURL: String
+    let latitude: Float
+    let longitude: Float
+    let createdAt: Date
+    let updatedAt: Date
+    let ACL: String
+}
