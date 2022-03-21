72098b7e7fcab65e0c59079949b3e7459304518c
Shir Bar Lev <shirblc@icloud.com>
2022-03-21 21:05:40 +0000

    Added initial map view controller

---
+++ onthemap/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  onthemap
+//
+//  Created by Shir Bar Lev on 21/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+    var sessionId: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
