a4c90c9b4a1113531c5ea7cba66e4975b8fa12ff
Shir Bar Lev <shirblc@icloud.com>
2022-03-26 19:55:41 +0000

    Added view for adding/editing user location

---
+++ onthemap/LocationEditViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LocationEditViewController.swift
+//  onthemap
+//
+//  Created by Shir Bar Lev on 26/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class LocationEditViewController: UIViewController {
+    
+}
