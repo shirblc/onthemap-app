c82dc2a566c4cdb148c7e63646a6890dced5a718
Shir Bar Lev <shirblc@icloud.com>
2022-03-26 17:23:05 +0000

    Added table view controller for the students list

---
+++ onthemap/StudentListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  StudentListViewController.swift
+//  onthemap
+//
+//  Created by Shir Bar Lev on 26/03/2022.
+//
+
+import Foundation
+import UIKit
+
+let callViewIdentifier = "onTheMapCV"
+
+class StudentListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // MARK: Variables & Constants
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    // MARK: UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.appDelegate.studentLocations?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let studentAtIndexPath = self.appDelegate.studentLocations?[indexPath.row]
+        var userCellView = tableView.dequeueReusableCell(withIdentifier: callViewIdentifier)
+        
+        // create the cell if there isn't an existing one
+        if userCellView == nil {
+            userCellView = UITableViewCell(style: .subtitle, reuseIdentifier: callViewIdentifier)
+        }
+        
+        // set up the cell's content
+        var cellContent = userCellView?.defaultContentConfiguration()
+        cellContent?.text = "\(studentAtIndexPath?.firstName ?? "") \(studentAtIndexPath?.lastName ?? "")"
+        cellContent?.secondaryText = studentAtIndexPath?.mediaURL
+        
+        userCellView?.contentConfiguration = cellContent
+        
+        return userCellView!
+    }
+    
+    
+}
