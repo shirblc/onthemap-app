53c95aedbeaa87a16785ceccb4265807935c9a09
Shir Bar Lev <shirblc@icloud.com>
2022-03-28 11:33:49 +0100

    Added view controller for adding a link to the user's location.

---
+++ onthemap/LinkPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LinkPostViewController.swift
+//  onthemap
+//
+//  Created by Shir Bar Lev on 28/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class LinkPostViewController: UIViewController {
+    var userLocation: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
